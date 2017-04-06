7a9b542868ddf471a1f1699e54aa390ebd889f62
Torsten <tboettjer@live.de>
2017-04-06 12:06:04 +0200

    Initial Uplaod

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostbyname("www.google.com")
+mysock.connect(host,80)
+
+message = "GET / HTTP/1.1\r\n\r\n"
+mysock.sendall(message)
