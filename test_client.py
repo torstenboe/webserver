f632284b50806e7454eca4803940fa93b59fdaec
Torsten <tboettjer@live.de>
2017-04-06 14:13:53 +0200

    Initial Uplaod

---
+++ test_client.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+
+try:
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+except socket.error:
+    print("Failed to create socket")
+    sys.exit()
+
+try:
+    host = 127.0.0.1
+
+except socket.gaierror:
+    print("Failed to get host")
+    sys.exit()
+
+mysock.connect((host, 1234))
+message = "GET / HTTP/1.1\r\n\r\n"
+
+try:
+    mysock.sendall(bytes(message, 'UTF-8'))
+
+except socket.gaierror:
+    print("Failed to send")
+    sys.exit()
+
+data = mysock.recv(1000)
+print(data)
+mysock.close()
