1a3da724db90db550a0387e4359f75a9bf418aae
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 11:28:58 +0200

    Part3

--- tweeting.py
+++ tweeting.py
@@ -8,8 +8,8 @@ A_SECRET = "..."
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
-            print("Found it!")
+            print("Ian G. Harris is popular!")
 
 stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
 
-stream.status.filter(track="Harris")
+stream.status.filter(track="Ian G. Harris")
