9b1b894d537ee72c7ef665aa544eb252a9a79fd9
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 11:57:09 +0200

    Part3

--- tweeting.py
+++ tweeting.py
@@ -9,10 +9,10 @@ count = 0
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
-        if 'text' in data && count < 3:
+        if ('text' in data) && (count < 3):
             count += count
             print("found")
-        else if 'text' in data && count == 3:
+        else if ('text' in data) && (count == 3):
             count = 0
             print("Ian G. Harris is popular!")
 
