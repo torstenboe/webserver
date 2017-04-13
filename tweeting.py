866a446a6a705e67cc702b55c6d294202a88d765
Torsten <tboettjer@live.de>
2017-04-13 10:13:37 +0200

    Initial Uplaod

--- tweeting.py
+++ tweeting.py
@@ -5,6 +5,11 @@ C_SECRET = "..."
 A_TOKEN = "..."
 A_SECRET = "..."
 
-api = Twython(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print("Found it!")
 
-api.update_status("Hi")
+stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
+
+stream.status.filter(track="Harris")
