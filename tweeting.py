ee58edbcb63dcda28f835dfff4565f340f454702
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 11:54:02 +0200

    Part3

--- tweeting.py
+++ tweeting.py
@@ -5,9 +5,15 @@ C_SECRET = "..."
 A_TOKEN = "..."
 A_SECRET = "..."
 
+count = 0
+
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
-        if 'text' in data:
+        if 'text' in data && count < 3:
+            count += count
+            print("found")
+        else if 'text' in data && count == 3:
+            count = 0
             print("Ian G. Harris is popular!")
 
 stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
