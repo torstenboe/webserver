fc97e6fc76ad0af6b1bae944f65b77582a9a01b7
torstenboe <torsten.boettjer@gmail.com>
2017-04-13 12:58:16 +0200

    Part3

--- tweeting.py
+++ tweeting.py
@@ -1,21 +1,20 @@
 from twython import TwythonStreamer
 
-C_KEY = "..."
-C_SECRET = "..."
-A_TOKEN = "..."
-A_SECRET = "..."
+C_KEY="..."
+C_SECRET="..."
+A_TOKEN="..."
+A_SECRET="..."
 
-count = 0
+count = 1
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
-        if ('text' in data) && (count < 3):
-            count += count
-            print("found")
-        else if ('text' in data) && (count == 3):
-            count = 0
+        global count
+        if ('text' in data) and (count < 3):
+            count = count + 1
+        elif ('text' in data) and (count <= 3):
             print("Ian G. Harris is popular!")
 
 stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
 
-stream.status.filter(track="Ian G. Harris")
+stream.statuses.filter(track="Ian G. Harris")
